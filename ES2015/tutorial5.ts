68179e39f19867d9c0f2728d7aacc617d02fb172
atulsharma1999 <atul.sharma@technocodz.com>
2021-05-25 15:16:41 +0530

    React

--- ES2015/tutorial5.ts
+++
@@ -1,9 +0,0 @@
-function greetPerson(name: string){
-    if(name === "Chandler"){
-        var greet = "Hello Chandler";
-    }else{
-        var greet = "Hi there";
-    }
-    console.log(greet);
-}
-greetPerson("Chandler");
