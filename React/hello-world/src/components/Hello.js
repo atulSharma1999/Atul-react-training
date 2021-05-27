03fe19483dd06988233bf38b61f3839b72e788ec
atulsharma1999 <atul.sharma@technocodz.com>
2021-05-27 14:08:41 +0530

    React

--- React/hello-world/src/components/Hello.js
+++ React/hello-world/src/components/Hello.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const Hello = () => {
-    return (
-     <div>
-        <h1>Hello Atul</h1>
-     </div>
-    )
+//     return (
+//      <div className='dummyclass'>
+//         <h1>Hello Atul</h1>
+//      </div>
+//     )
+       return React.createElement(
+           'div',
+            {id: 'hello', className: 'dummyclass'}, 
+            React.createElement('h1', null, 'Hello Atul'))
 }
 
 export default Hello
