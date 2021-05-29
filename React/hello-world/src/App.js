47252e312118832ed145d92bb68ce1bf751b7870
atulsharma1999 <atul.sharma@technocodz.com>
2021-05-29 13:41:00 +0530

    React

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -5,12 +5,18 @@ import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind'
 
 
 class App extends Component {
   render() {
   return (
     <div className="App">
+      <EventBind />
+      {/* <FunctionClick /> */}
+      {/* <ClassClick /> */}
       {/* <Counter /> */}
       {/* <Message />  */}
     {/* <Greet name="Bruce"  heroName="Batman">
@@ -19,8 +25,8 @@ class App extends Component {
     <Greet name="Clark"  heroName="Superman">
         <button>Action</button>
   </Greet> */}
-    <Greet name="Diana"  heroName="Wonder woman" />
-    <Welcome name="Bruce"  heroName="Batman"/>
+    {/* <Greet name="Diana"  heroName="Wonder woman" />
+    <Welcome name="Bruce"  heroName="Batman"/> */}
       {/* <Welcome name="Clark"  heroName="Superman" />
       <Welcome name="Diana"  heroName="Wonder woman"/>  */}
     {/* <Hello /> */}
