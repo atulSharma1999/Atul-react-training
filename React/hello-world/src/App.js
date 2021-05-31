5739d5a658716567f08ebbc5bb3e919c9eac67bf
atulsharma1999 <atul.sharma@technocodz.com>
2021-05-31 13:01:20 +0530

    React

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -9,14 +9,17 @@ import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
 import EventBind from './components/EventBind'
 import ParentComponent from './components/ParentComponent'
-import UserGreeting from './components/UserGreeting';
+import UserGreeting from './components/UserGreeting'
+import NameList from './components/NameList'
+
 
 
 class App extends Component {
   render() {
   return (
     <div className="App">
-      <UserGreeting />
+      <NameList />
+      {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
       {/* <EventBind /> */}
       {/* <FunctionClick /> */}
