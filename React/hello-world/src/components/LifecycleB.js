f732ce2292d695d34d6c2a840cbd7cd075a3c974
atulsharma1999 <atul.sharma@technocodz.com>
2021-06-01 15:06:56 +0530

    React

--- React/hello-world/src/components/LifecycleB.js
+++ React/hello-world/src/components/LifecycleB.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import LifecycleB from './LifecycleB'
 
-class LifecycleA extends Component {
+class LifecycleB extends Component {
     constructor(props) {
         super(props)
     
