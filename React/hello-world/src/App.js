3854d8c71b65d80668db0dd38702f6968530edb8
atulsharma1999 <atul.sharma@technocodz.com>
2021-06-03 13:24:18 +0530

    React

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -19,13 +19,14 @@ import LifecycleA from './components/LifecycleA';
 import FragmentDemo from './components/FragmentDemo';
 import PureComp from './components/PureComp';
 import ParentComp from './components/ParentComp';
-
+import RefsDemo from './components/RefsDemo';
 
 class App extends Component {
   render() {
   return (
     <div className="App">
-      <ParentComp />
+      <RefsDemo /> 
+      {/* <ParentComp /> */}
       {/* <PureComp /> */}
       {/* <FragmentDemo /> */}
       {/* <LifecycleA /> */}
